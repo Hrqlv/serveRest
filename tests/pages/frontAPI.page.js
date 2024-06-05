0ee9615c6e9872e798f767d226f36f148a65bfc4
Hrqlv <henriquelopesvelozo@gmail.com>
2024-06-05 17:37:13 -0300

    Realizado api de login com sucesso pelo get e de usuario com get, post e getID

--- tests/pages/frontAPI.page.js
+++ tests/pages/frontAPI.page.js
@@ -2,22 +2,67 @@ import { expect, request } from "@playwright/test";
 
 export class ServicesAPI {
     constructor(authToken) {
-        this.authToken = authToken
+        this.authToken = authToken;
+        this.id = '0uxuPY0cbmQhpEz1',
+        this.urlBase = 'https://serverest.dev'
     }
-    
-    async postLogin(user = string, password = string) {
+
+    async postLogin(user, password) {
+        const context = await request.newContext();
+        const response = await context.post(`${this.urlBase}/login`, {
+                data: {
+                    email: user, 
+                    password: password
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.authToken}` 
+                },
+            });
+            expect(response.status(), `Request (/login) failed\nStatus: ${(response.status())} ${response.statusText()}`).toBe(200)
+            return response;
+    }
+
+    async getUsuarios() {
         const context = await request.newContext()
-        const response = await context.post(`/login`, {
-            data: {
-                "userName": user,
-                "password": password
-            },
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        return response
-        // expect(response.status(), `Request (Account/v1/Login) failed\nStatus: ${(response.status())} ${response.statusText()}`).toBe(200)
-        // return response.json();
+        const response = await context.get(`${this.urlBase}/usuarios`)
+        return response;
     }
+
+    async postUsuarios(nome, email, senha, adm) {
+        const context = await request.newContext()
+        const response = await context.post(`${this.urlBase}/usuarios`, {
+            data: {
+                nome: nome,
+                email: email,
+                password: senha,
+                administrador: adm
+              },
+        })
+        expect(response.status(), `Request (/usuarios) failed\nStatus: ${(response.status())} ${response.statusText()}`).toBe(201)
+        return response;
+    }
+
+    async getUsuariosID() {
+        const context = await request.newContext()
+        const response = await context.get(`${this.urlBase}/usuarios/${this.id}`)
+        return response
+    }
+
+    // async clearCarrinho() {
+    //     const context = await request.newContext({
+    //         extraHTTPHeaders: {
+    //             'Authorization': `Bearer ${this.authToken}`,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     });
+    //     const response = await context.delete(`${this.urlBase}/carrinhos/concluir-compra`);
+    //     return response;
+    // }
+
+    // async deleteUsuarioID() {
+    //     const context = await request.newContext()
+    //     const response = await context.delete(`${this.urlBase}/usuarios/${this.id}`)
+    //     return response
+    // }
 }
--- tests/specs/front-api/serverRest-API.spec.js
+++ tests/specs/front-api/serverRest-API.spec.js
@@ -1,29 +1,63 @@
 const { test, expect } = require('@playwright/test');
+const { faker } = require('@faker-js/faker');
 import { ServicesAPI } from '../../pages/frontAPI.page';
 
-let servicesApi = new ServicesAPI();
+let servicesApi;
+let userId;
 
-test.describe('API Tests @API', async () => {
-  test.beforeEach(async({ page }) => {
-    await page.goto('https://serverest.dev/login')
+const nome = faker.name.fullName();
+const email = faker.internet.email();
+const senha = faker.internet.password();
+const adm = 'true';
+
+test.describe('API Tests @API', () => {
+  test.beforeEach(async ({ page }) => {
     servicesApi = new ServicesAPI();
   });
 
-  test('Realizar Login com sucesso @APILOGIN', async ({ page }) => {
-    await test.step('Preencher email e senha validos para o login com sucesso', async () => {
+  test('Realizar Login com sucesso com API', async ({ page }) => {
+    await test.step('Preencher email e senha validos para o login com sucesso - POST', async () => {
         const postSignin = await servicesApi.postLogin('fulano@qa.com', 'teste');
         const signinBody = await postSignin.json();
-        expect(signinBody.success).toEqual(true);
-    });
-
-    await test.step('Login and validate user', async () => {
-        const loginResponse = await servicesApi.postLogin('fulano@qa.com', 'teste');
-        expect(loginResponse.status()).toBe(200);
-        // const loginResponseJson = await loginResponse.json();
-        // await validateJsonSchema('POST_Login', loginResponseJson);
-        // expect(loginResponseJson.username).toBe(user.userName)
-        // expect(loginResponseJson.password).toBe(user.password)
-        // expect(loginResponseJson.userId).toBe(userID)
+        expect(signinBody.message).toEqual('Login realizado com sucesso');
+        expect(postSignin.status()).toBe(200);
     });
   });
+
+  test('Realizar fluxo de usuarios com API', async ({ page }) => {
+    await test.step('Obter os dados do usuario - GET', async () => {
+      const usuariosResponse = await servicesApi.getUsuarios();
+      const usuarioBody = await usuariosResponse.json();
+      expect(usuariosResponse.status()).toBe(200);
+      expect(usuarioBody).toHaveProperty('usuarios');
+      expect(usuarioBody.usuarios.length).toBeGreaterThan(0); 
+    })
+
+    await test.step('Cadastrar um usuário - POST', async () => {
+      const postUsuarios = await servicesApi.postUsuarios(nome, email, senha, adm);
+      const usuariosBody = await postUsuarios.json();
+      expect(usuariosBody.message).toEqual('Cadastro realizado com sucesso');
+      userId = usuariosBody._id;
+      expect(postUsuarios.status()).toBe(201)
+    });
+
+    await test.step('Buscar o usuairo pelo id', async () => {
+      const usuariosIDResponse = await servicesApi.getUsuariosID();
+      const usuarioIDBody = await usuariosIDResponse.json();
+      expect(usuarioIDBody).toHaveProperty('_id')
+      expect(usuariosIDResponse.status()).toBe(200);
+    })
+
+    // await test.step('Limpar o carrinho do usuário', async () => {
+    //   const clearCarrinhoResponse = await servicesApi.clearCarrinho(userId);
+    //   expect(clearCarrinhoResponse.status()).toBe(200);
+    // });
+
+    // await test.step('Excluir o usuairo pelo id', async () => {
+    //   const deleteUsuarioResponse = await servicesApi.deleteUsuarioID(userId);
+    //   const deleteUsuarioBody = await deleteUsuarioResponse.json();
+    //   expect(deleteUsuarioBody.message).toEqual('Registro excluído com sucesso');
+    //   expect(deleteUsuarioResponse.status()).toBe(200)
+    // })
+});
 });
